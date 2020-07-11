2548af8a0659fd1f4c8e767f4f194105642d3a33
Joseph H <hawkejo@users.noreply.github.com>
2020-07-11 08:07:22 -0600

    Initial commit

---
+++ vga_controller.srcs/sources_1/new/vga_640x480.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 07/11/2020 08:04:54 AM
+// Design Name: 
+// Module Name: vga_640x480
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module vga_640x480(
+
+    );
+endmodule
---
+++ vga_controller.srcs/sources_1/new/vga_top.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 07/11/2020 08:04:54 AM
+// Design Name: 
+// Module Name: vga_top
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module vga_top(
+
+    );
+endmodule
