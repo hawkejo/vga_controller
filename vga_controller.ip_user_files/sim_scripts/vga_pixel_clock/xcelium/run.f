8419ae46bc1a698b80c8c8e490ab255be2b93ad9
Joseph H <hawkejo@users.noreply.github.com>
2020-09-15 12:49:41 -0400

    cleared branch

--- vga_controller.ip_user_files/sim_scripts/vga_pixel_clock/xcelium/run.f
+++
@@ -1,8 +0,0 @@
--makelib xcelium_lib/xil_defaultlib \
-  "../../../../vga_controller.srcs/sources_1/ip/vga_pixel_clock/vga_pixel_clock_clk_wiz.v" \
-  "../../../../vga_controller.srcs/sources_1/ip/vga_pixel_clock/vga_pixel_clock.v" \
--endlib
--makelib xcelium_lib/xil_defaultlib \
-  glbl.v
--endlib
-
